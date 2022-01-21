fb3d7616155bd60e8cd8591b9e8603b593354a03
Laurynas Juzva <laurynas.juzva@gmail.com>
2022-01-21 13:40:26 -0800

    starting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Footer from './Components/Header/Header';
+import Header from './Components/Footer/Footer';
+import Home from './Pages/Home/Home'
+
+
 
 function App() {
   return (
-   <h1>Lets Fucking go</h1>
-  );
+    <div className='App'>
+      <Router>
+      <Switch>
+      <Header />
+      <Route path='/' exact component={Home} />
+
+
+
+      
+      </Switch>
+
+
+      <Footer />
+      </Router>
+        </div>
+  )
+
+
 }
 
 export default App;
