f8097931563f3cbbf6b2eab4c1b0c0013bea77a2
Laurynas Juzva <laurynas.juzva@gmail.com>
2022-01-25 14:44:52 -0800

    working on home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import './Home.css'
+import plastick from '../../Assets/Images/Plastick.png'
+
 
 
 const Home = () => {
   return (
     <div className="Home">
-    
-    <h1 className="text-center" style={{paddingTop: "30%"}}>
-      Home
-    </h1>
-    
+          <div className="Home__Banner">
+          <h1 className = "Home__h1">Medical Marketing <br/> & Website Design <br/>
+            <span className="emphasis-1">Technology-lead marketing that delivers unparalleled results.</span> 
+          </h1>
+          <a href = "#more">
+      <img id = "Home__img" src={plastick} alt="Home art"/> </a>
+    </div>
+
+
+
+
   </div>
   )
 }
