27a961624439cae43baaa18e9bf4a73dce121076
Laurynas Juzva <laurynas.juzva@gmail.com>
2022-01-25 16:41:21 -0800

    slider works

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,22 +1,14 @@
 import React from "react";
+// import MyComponent from "../components/MyComponent";
+import Marquee from "react-fast-marquee";
 import './Slider.css'
-
-const root = document.documentElement;
-const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
-const marqueeContent = document.querySelector("ul.marquee-content");
-
-root.style.setProperty("--marquee-elements", marqueeContent.children.length);
-
-for(let i=0; i<marqueeElementsDisplayed; i++) {
-  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
-}
 
 
 function Slider() {
   return (
-
-    <div className="marquee">
-    <ul className="marquee-content">
+<Marquee>
+    <div className="Slider">
+    <ul className="Slider-content">
       <li><i className="fab fa-github"></i></li>
       <li><i className="fab fa-codepen"></i></li>
       <li><i className="fab fa-free-code-camp"></i></li>
@@ -31,12 +23,9 @@ function Slider() {
       <li><i className="fab fa-android"></i></li>
     </ul>
   </div>
+
+  </Marquee>
   )
-
-
-
-
-
 }
 
 export default Slider;
