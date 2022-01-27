2999c2af237bbf8c72b2876666280c9037a0f122
Laurynas Juzva <laurynas.juzva@gmail.com>
2022-01-27 14:14:04 -0800

    second slider is going places

--- src/Components/ServiceSlider/ServiceSlider.js
+++ src/Components/ServiceSlider/ServiceSlider.js
@@ -4,24 +4,44 @@ import './ServiceSlider.css'
 
 function ServiceSlider() {
     return (
-        <Marquee speed="20" pauseOnHover={true}>
-            <div className="">
-            <ul className="">
-              <li><i className="fab fa-github"></i></li>
-              <li><i className="fab fa-codepen"></i></li>
-              <li><i className="fab fa-free-code-camp"></i></li>
-              <li><i className="fab fa-dev"></i></li>
-              <li><i className="fab fa-react"></i></li>
-              <li><i className="fab fa-vuejs"></i></li>
-              <li><i className="fab fa-angular"></i></li>
-              <li><i className="fab fa-node"></i></li>
-              <li><i className="fab fa-wordpress"></i></li>
-              <li><i className="fab fa-aws"></i></li>
-              <li><i className="fab fa-docker"></i></li>
-              <li><i className="fab fa-android"></i></li>
-            </ul>
-          </div>
-        
+        <Marquee speed="50" pauseOnHover={true}>
+          <div className="ServiceSlider__row">
+      <div className="col-sm-6 zero">
+              <div class="service-slide" 
+              onclick="window.location.href='//';" 
+              data-uw-styling-context="true">
+                <div class="service-box" data-uw-styling-context="true">
+                  <div class="service-icon-3" data-uw-styling-context="true">
+                    </div>
+                    <h2 class="service-name" data-uw-styling-context="true">Mobile Design
+                    </h2>
+                    <div class="service-text" data-uw-styling-context="true">
+                      More than half your traffic is mobile. We design and build custom websites that connect with your future clients on any screen.
+                      </div> 
+                      <a href="/" class="button" tabindex="-1" data-uw-styling-context="true">
+                        Learn More
+                      </a>
+                      </div>
+                      </div>
+              <div class="service-slide">
+                <div class="service-box">
+                  <div class="service-icon-1">
+                    </div>
+                    <h2 class="service-name">In-Depth Content
+                    </h2>
+                    <div class="service-text">Google rewards authority. We research your topics and consult with you to write 1,500+ word articles which build your search rank to increase organic leads.
+                    </div>
+                     <a href="/" class="button" tabindex="-1">
+                       Learn More
+                       </a>
+                       </div>
+                       </div>
+
+
+
+
+                       </div>
+          </div>        
           </Marquee>
           )
         }
